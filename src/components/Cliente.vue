167eb49861062a8ae5f02dae19a4ba5fbc815a2b
Trouttt <44469295+Trouttt@users.noreply.github.com>
2021-02-20 18:21:15 -0300

    Added: Cliente.vue and Produto.vue

---
+++ src/components/Cliente.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="cliente">
+        <h2>Cliente aqui! - Componente aqui</h2>
+        <h3>fsfsfs</h3>
+        <hr>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    #cliente{
+        color:blue;
+        background:linear-gradient(to bottom, rgba(114, 113, 113,0.9), rgba(114, 113, 113,0.7),rgba(114, 113, 113,0.5))
+    }
+</style>
---
+++ src/components/Produto.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>Product</h2>
+        <p>Product description</p>
+        <input type="text" placeholder="price">
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    div{
+        width:100%;
+        height:30vw;
+    }
+</style>
