a90bef7871246ca4e15fc4ee3bc960454abb8b89
Trouttt <44469295+Trouttt@users.noreply.github.com>
2021-02-24 13:40:53 -0300

    V-for

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,10 @@
   <div id="app">
     <h1>Guia Clientes</h1>
     <input type="text" v-model="user.name"/>
-    <Cliente :client="user" :showAge="true"/>
-    <Cliente :client="user" :showAge="false"/>
-    <Cliente :client="user" :showAge="true"/>
+    <div v-for="(user, index) in users" :key="user.id">
+      <h3>{{ index }}</h3>
+      <Cliente :client="user"/>
+    </div>
   </div>
 </template>
 
@@ -18,10 +19,37 @@ export default {
       user:{
         name: "Victor",
         number:"(22)5129219",
-        email:"aiaipapito@gmail.com",
+        email:"fsafsito@gmail.com",
         age:12,
         description:"eita maluco doideira"
-      }
+      },
+      users:[
+        {
+          id:1,
+
+          name: "Leonardo",
+          number:"(55)959219",
+          email:"aiaigsagsaqe@gmail.com",
+          age:20,
+          description:"arroz doce"
+        },
+        {
+          id:3,
+          name: "Tiktok",
+          number:"(20)5129219",
+          email:"attttttt@gmail.com",
+          age:30,
+          description:"juvenal"
+        },
+        {
+          id:10,
+          name: "Vfsafastor",
+          number:"(22)5114219",
+          email:"aiairqwrwqpito@gmail.com",
+          age:42,
+          description:"TATIANE MEU AMOR"
+        }
+      ]
     }
   },
   name: 'App',
--- src/components/Cliente.vue
+++ src/components/Cliente.vue
@@ -1,29 +1,49 @@
 <template>
-    <div id="cliente">
+    <div :class="{'client': !isPremium, 'client-premium': isPremium}">
         <h4>Name: {{client.name}}</h4>
         <p>Description: {{client.description}}</p>
         <p>Number: {{client.number}}</p>
         <p>Email: {{client.email}}</p>
         <p v-if="showAge == true">Age: {{client.age}}</p>
-        <p v-else-if="showAge == false"></p>
         <p v-else> O usuário não tem idade, é doido</p>
+
+        <button @click="changeColor($event)">Mudar cor!</button>
+
     </div>
     
 </template>
 
 <script>
 export default {
+    data(){
+        return{
+            isPremium: true
+        }
+    },
     props: {
         client:Object,
         showAge: Boolean
+    },
+    methods:{
+        changeColor: function($event){
+            console.log($event); 
+            this.isPremium = !this.isPremium;
+        }
     }
 }
 </script>
 
 <style scoped>
-    #cliente{
+    .client{
         color:blue;
         background-color:#ECE5E3;
+        padding:1vw;
+        margin-bottom:1vw;
+    }
+
+    .client-premium{
+        background-color:black;
+        color:yellow;
         padding:1vw;
         margin-bottom:1vw;
     }
